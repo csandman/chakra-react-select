9e6e38f4b4dc7627e0226b859cdaf582ff4ca816
Chris Sandvik <chris.sandvik@gmail.com>
2021-10-25 14:33:11 -0400

    Add Chakra UI Storybook plugin

--- src/stories/Select.stories.js
+++ src/stories/Select.stories.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import Select from "../select";
 
 export default {
@@ -9,22 +9,20 @@ export default {
 };
 
 export const DefaultSelect = () => (
-  <ChakraProvider>
-    <Container maxW="400px">
-      <Select
-        isMulti
-        options={[
-          {
-            label: "I can't be removed",
-            value: "fixed",
-            isFixed: true,
-          },
-          {
-            label: "I can be removed",
-            value: "not-fixed",
-          },
-        ]}
-      />
-    </Container>
-  </ChakraProvider>
+  <Container>
+    <Select
+      isMulti
+      options={[
+        {
+          label: "I can't be removed",
+          value: "fixed",
+          isFixed: true,
+        },
+        {
+          label: "I can be removed",
+          value: "not-fixed",
+        },
+      ]}
+    />
+  </Container>
 );
