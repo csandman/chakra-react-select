b98e80756660f924f3e0b95c39254b5475f5d6e1
Chris Sandvik <chris.sandvik@gmail.com>
2022-01-10 16:03:56 -0500

    Rename the exported `components` prop to `chakraComponents`

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ export { default as Select } from "./select";
 export { default as CreatableSelect } from "./creatable-select";
 export { default as AsyncSelect } from "./async-select";
 export { default as AsyncCreatableSelect } from "./async-creatable-select";
-export { default as components } from "./chakra-components";
+export { default as chakraComponents } from "./chakra-components";
 export * from "./types";
 
 export * from "react-select";
