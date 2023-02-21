073a1551175f0b76ef139068bf48767ab5bdf5db
Chris Sandvik <chris.sandvik@gmail.com>
2023-02-21 16:26:21 -0500

    Add the option to pass a breakpoint object/array for the `size` prop

--- src/use-chakra-select-props.ts
+++ src/use-chakra-select-props.ts
@@ -1,8 +1,39 @@
 import { useFormControl } from "@chakra-ui/form-control";
+import { useBreakpointValue } from "@chakra-ui/media-query";
 import { useTheme } from "@chakra-ui/system";
 import type { GroupBase, Props } from "react-select";
 import chakraComponents from "./chakra-components";
-import type { SelectedOptionStyle, Size, TagVariant, Variant } from "./types";
+import type {
+  SelectedOptionStyle,
+  Size,
+  SizeProp,
+  TagVariant,
+  Variant,
+} from "./types";
+
+/** A typeguard to ensure the default size on the Input component is valid. */
+const isSize = (size: unknown): size is Size => {
+  const isString = typeof size === "string";
+  return isString && ["sm", "md", "lg"].includes(size);
+};
+
+const getDefaultSize = (size: unknown): Size => {
+  if (isSize(size)) {
+    return size;
+  }
+
+  if (size === "xs") {
+    return "sm";
+  }
+
+  // This shouldn't be necessary but it might help the size get closer to the
+  // user's goal if they have `xl` as a custom size.
+  if (size === "xl") {
+    return "lg";
+  }
+
+  return "md";
+};
 
 const useChakraSelectProps = <
   Option,
@@ -34,12 +65,11 @@ const useChakraSelectProps = <
   const chakraTheme = useTheme();
   const { variant: defaultVariant, size: defaultSize } =
     chakraTheme.components.Input.defaultProps;
+  const realDefaultSize = getDefaultSize(defaultSize);
 
-  /**
-   * Combine the props passed into the component with the props that can be set
-   * on a surrounding form control to get the values of `isDisabled` and
-   * `isInvalid`
-   */
+  // Combine the props passed into the component with the props that can be set
+  // on a surrounding form control to get the values of `isDisabled` and
+  // `isInvalid`
   const inputProps = useFormControl({
     id: inputId,
     isDisabled,
@@ -54,17 +84,19 @@ const useChakraSelectProps = <
   const realMenuIsOpen =
     menuIsOpen ?? (inputProps.readOnly ? false : undefined);
 
-  /** Ensure that the size used is one of the options, either `sm`, `md`, or `lg` */
-  let realSize: Size = size ?? (defaultSize === "xs" ? "sm" : defaultSize);
-  const sizeOptions: Size[] = ["sm", "md", "lg"];
-  if (!sizeOptions.includes(realSize)) {
-    realSize = "md";
-  }
+  // Ensure that the size used is one of the options, either `sm`, `md`, or `lg`
+  const definedSize: SizeProp = size ?? realDefaultSize;
+  // Or, if a breakpoint is passed, get the size based on the current screen size
+  const realSize: Size =
+    useBreakpointValue<Size>(
+      typeof definedSize === "string" ? [definedSize] : definedSize,
+      {
+        fallback: "md",
+      }
+    ) || defaultSize;
 
-  /**
-   * Ensure that the tag variant used is one of the options, either `subtle`,
-   * `solid`, or `outline` (or undefined)
-   */
+  // Ensure that the tag variant used is one of the options, either `subtle`,
+  // `solid`, or `outline` (or undefined)
   let realTagVariant: TagVariant | undefined = tagVariant;
   const tagVariantOptions: TagVariant[] = ["subtle", "solid", "outline"];
   if (tagVariant !== undefined) {
@@ -73,14 +105,14 @@ const useChakraSelectProps = <
     }
   }
 
-  /** Ensure that the selected option style is either `color` or `check` */
+  // Ensure that the selected option style is either `color` or `check`
   let realSelectedOptionStyle: SelectedOptionStyle = selectedOptionStyle;
   const selectedOptionStyleOptions: SelectedOptionStyle[] = ["color", "check"];
   if (!selectedOptionStyleOptions.includes(selectedOptionStyle)) {
     realSelectedOptionStyle = "color";
   }
 
-  /** Ensure that the color used for the selected options is a string */
+  // Ensure that the color used for the selected options is a string
   let realSelectedOptionColor: string = selectedOptionColor;
   if (typeof selectedOptionColor !== "string") {
     realSelectedOptionColor = "blue";
