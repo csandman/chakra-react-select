e73fab0e16f612e567c1a8ebbd08e51cfdf384ee
csandman <chris@getcuebox.com>
2023-05-03 13:39:37 -0400

    Simplify babel config and switch to preconstruct for the build process

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,7 @@
-const { BABEL_ENV } = process.env;
-const isCjs = BABEL_ENV !== undefined && BABEL_ENV === "cjs";
-
 module.exports = {
-  plugins: [
-    "@chakra-ui/babel-plugin",
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-  ],
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      {
-        bugfixes: true,
-        modules: isCjs ? "commonjs" : false,
-        loose: true,
-      },
-    ],
+    "@babel/preset-env",
   ],
 };
