ed24ffeb6b3673652d2a05906e311622cd85ae13
csandman <chris@getcuebox.com>
2023-07-24 17:10:02 -0400

    Fix the height setting on the control

--- src/chakra-components/control.tsx
+++ src/chakra-components/control.tsx
@@ -45,13 +45,22 @@ const Control = <
 
   const size = useSize(sizeProp);
   const {
-    field: { h: minH, ...fieldStyles },
+    field: { height, h, ...fieldStyles },
   } = useMultiStyleConfig("Input", {
     size,
     variant,
     focusBorderColor,
     errorBorderColor,
   });
+
+  /**
+   * `@chakra-ui/theme@3.2.0` introduced a breaking change that switched from using `h` to `height` for the Input sizing.
+   *
+   * We need to keep checking for either to maintain backwards compatibility.
+   *
+   * @see {@link https://github.com/chakra-ui/chakra-ui/releases/tag/%40chakra-ui%2Ftheme%403.2.0}
+   */
+  const minH = height || h;
 
   const initialSx: SystemStyleObject = {
     ...fieldStyles,
@@ -62,6 +71,7 @@ const Control = <
     flexWrap: "wrap",
     padding: 0,
     overflow: "hidden",
+    height: "auto",
     minH,
     ...(isDisabled ? { pointerEvents: "none" } : {}),
   };
