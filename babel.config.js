a9c3790b3951f97bc30b77bbca435c0dd6207a50
csandman <chris@getcuebox.com>
2023-09-29 15:47:54 -0400

    Add vite sandbox

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = {
     ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
   presets: [
-    ["@babel/preset-typescript", {}],
+    "@babel/preset-typescript",
     "@babel/preset-react",
     [
       "@babel/preset-env",
