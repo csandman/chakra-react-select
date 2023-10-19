5c1f5eeadaf58999abf30cab659a6624f23f8c38
csandman <chris@getcuebox.com>
2023-10-18 22:32:35 -0400

    Remove unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,6 @@ const { BABEL_ENV } = process.env;
 const isCjs = BABEL_ENV !== undefined && BABEL_ENV === "cjs";
 
 module.exports = {
-  plugins: [
-    "@chakra-ui/babel-plugin",
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-  ],
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
