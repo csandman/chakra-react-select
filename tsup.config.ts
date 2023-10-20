84297732f630364a8655823b00ad2a13477440e2
csandman <chris@getcuebox.com>
2023-10-19 20:24:08 -0400

    Switch to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src"],
+  format: ["cjs", "esm"],
+  target: "es2019",
+  sourcemap: true,
+  dts: true,
+});
