15647e51a37c0b4cc04adf8acb9cc9c8fdd46c38
csandman <chris@getcuebox.com>
2024-05-01 21:26:39 -0400

    Attempt to switch to rollup

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,3 @@
-const { BABEL_ENV } = process.env;
-const isCjs = BABEL_ENV !== undefined && BABEL_ENV === "cjs";
-
 module.exports = {
-  plugins: [
-    "@chakra-ui/babel-plugin",
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-  ],
-  presets: [
-    "@babel/preset-typescript",
-    "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      {
-        bugfixes: true,
-        modules: isCjs ? "commonjs" : false,
-        loose: true,
-      },
-    ],
-  ],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
 };
