dcb7eedcf0792acab061c2adb701d39119089deb
csandman <chris@getcuebox.com>
2024-09-04 21:28:13 -0400

    Add explicit external tags

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,4 +9,17 @@ export default defineConfig({
   dts: true,
   minify: true,
   treeshake: true,
+  external: [
+    "react",
+    "react-dom",
+    "react-select",
+    "@chakra-ui/form-control",
+    "@chakra-ui/icon",
+    "@chakra-ui/layout",
+    "@chakra-ui/media-query",
+    "@chakra-ui/menu",
+    "@chakra-ui/react",
+    "@chakra-ui/spinner",
+    "@chakra-ui/system",
+  ],
 });
