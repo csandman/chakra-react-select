1b3b9e22ffc11be05103a26541198424953ab4af
csandman <chris@getcuebox.com>
2024-10-09 15:09:05 -0400

    Undo tsup config change

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "tsup";
 
 export default defineConfig({
