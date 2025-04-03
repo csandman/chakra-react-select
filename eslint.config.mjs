e713260b90b91d459fb54ab8032991f67c5c0e12
csandman <chris@getcuebox.com>
2025-04-02 22:34:57 -0400

    Upgrade to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,95 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/",
+      "**/dist/",
+      "codemod/**/*.js",
+      "codemod/**/*.d.ts",
+      "demo/**/*",
+      "eslint.config.mjs",
+    ],
+  },
+  importPlugin.flatConfigs.recommended,
+  {
+    settings: {
+      react: { version: "19" },
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      curly: ["error", "all"],
+      "no-console": "error",
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "arrow-function",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+      "react/jsx-filename-extension": [
+        "error",
+        {
+          extensions: [".tsx"],
+        },
+      ],
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: true,
+        },
+      ],
+      "@typescript-eslint/no-deprecated": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/select/*.tsx"],
+    rules: {
+      "react/display-name": "off",
+    },
+  },
+  {
+    files: ["codemod/**/*.ts"],
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  prettier
+);
