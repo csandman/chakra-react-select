0f10df574d01e0a980850264b76e3a0060bfc071
csandman <chris@getcuebox.com>
2025-11-12 23:48:34 -0500

    Update all dependencies and add eslint-plugin-unicorn

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  resolve: {
+    dedupe: ["react", "react-dom", "@emotion/react"],
+  },
 });
